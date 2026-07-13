a5407049b530698f2cf72d7915e0dde2828be8e2
Nguyen Phuoc Thanh Lam <phuocthanhlamnguyen@gmail.com>
2026-07-13 16:03:49 +0700

    Update main.c
    Just update it

--- code/code_file/main.c
+++ code/code_file/main.c
@@ -1,46 +1,4 @@
-/*
-Welcome to my project now today is 08/07/2026 and in 10:44 PM, I wrote this so future readers can read my projects
-
-Now I'm currently in grade 5, and I'm writing this to calm myself down as I like writing code
-
-I may put this at MIT license so that others can fix me
-
-And now this is some rules you should follow when writing code outside the license
-
-Lastly email me or write in github issues if you spot bugs or if you want a safety mechanism , but as there's lot's of spam on the inbox, it may take a few days to find the issues
-
-Also, I'm currently only making small updates, but small ones may come bigger and big ones may break
-
-By the way, the dates is incorrect as I may not change it often, so check using Github or git =)
-
-Also, if you want an acceptable update or upgrade like GUI, TUI , etc... , let me know =)
-
-Rules:
-And I may need some testers please =)
-
-And in the community chat on github, please don't cause cyberbullying, crimes, politics, flexing, or just downright
-
-adult content, as I'm not on the mood to do so
-
-Rules:
-
-Don't change the comments without permission
-During the comments even when have permission don't swear or tell any misinformation others about something or someone
-Don't change the typedef struct, it's very important, if you don't have permission
-Don't get this wrong: return 1 equals to memory error or human failure return 2 is the others error you can or will fill in
-If you want to contact me my email is phuocthanhlamnguyen@gmail.com
-Use the email to tell me that there's changes , but it may takes a few days
-Don't change the code to other languages , if you have no permission
-Changing code also needs comments, so others can understand and learn
-Flexing by changing code or other means in this repo is a big no-no
-No AI or vibecoding is allowed, as I don't want any "AI-slop" or people paying tokens for this small projects
-It's somewhat forced, but compile this in Linux, or UNIX like system (POSIX, MacOS), but if you're on a Windows device then just use WSL(safer but slow and room of errors) or dual-boot(more rooms for errors but dual-booting is fast in some instances and for long-term use or heavy-use, yeah use it)
-On the Github repo, go to master, not main so that you found the right code to download, compile or change
-If you don't want a way of email, you can call me by +84 396 656 656 (Yes, I'm Vietnamese) , but email is more preferred
-Don't delete the main.c (aka. main code), even though I have git backup, but still don't dare to delete it
-Done!
-*/
-
+//Welcome to my projects!
 #include <stdio.h> // Print text, and other purposes
 #include <unistd.h> //For time purposes and for UNIX system call(aka. syscall)
 #include <string.h> //To make a loop (idk why my thoughts tell me that)
@@ -117,7 +75,7 @@ int main(){
             //Add a milliseconds
             master_ms++;
             //Check if epochs is more or equal to a day
-            if (epoch.epochs >= 864000.0f){
+            if (epoch.epochs >= 86400.0f){
                 //Set everything back to 0 =)
                 master_ms = 0;
             }
